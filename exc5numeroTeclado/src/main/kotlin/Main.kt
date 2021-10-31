74c2ede20c5eb49743c333df88046e94bf4cd38e
henriquelanzoni <ricklanzoni@gmail.com>
2021-10-31 20:34:44 -0300

    exercicio 5 incluido

--- exc5numeroTeclado/src/main/kotlin/Main.kt
+++ exc5numeroTeclado/src/main/kotlin/Main.kt
@@ -9,10 +9,39 @@ fun main(args: Array<String>) {
      */
 
 
-    var soma = 0
-    var  media = 0
+    var total = 0
     var menor = 0
     var maior= 0
+    var num = 1
+    var cont = 0
+
+    println("Digite os números para serem calculados: ")
+
+    do {
+        num=readLine()!!.toInt()
+        total += num                    //total = total + num
+        cont ++                         //cont = cont + 1
+        if (num != 0){
+            if (num > maior){
+                maior = num             // = recebe| == igual
+            }else if (num < menor){
+                menor = num
+            }
+            if (menor==0){
+                menor=num
+            }
+            if (maior==0){
+                maior=num
+            }
+        }
+    }while (num != 0)
+    println("O total é: $total" +
+            "\nA média é: ${total/(cont-1).toDouble()}" +
+            "\nO maior numero é: $maior" +
+            "\nO menor número é: $menor")
+
+
+
 
 
 }
